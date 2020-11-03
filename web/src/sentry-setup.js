7736178c5290606a1d70c088a3ac5c8f09cf0a36
Louay Alakkad <louay_alakkad@mckinsey.com>
2020-11-03 19:08:11 +0000

    Switch to plain react app

---
+++ web/src/sentry-setup.js
@@ -0,0 +1,15 @@
+import * as Sentry from '@sentry/browser';
+import { Integrations } from '@sentry/tracing';
+
+if (process.env.REACT_APP_SENTRY_DSN) {
+  Sentry.init({
+    dsn: process.env.REACT_APP_SENTRY_DSN,
+    integrations: [
+      new Integrations.BrowserTracing(),
+    ],
+
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 1.0,
+  });
+}
